cfc72c9f596834488a11f61e4d959d630e7c6b54
Boris Gribovskiy <boris.gribovskiy@cyberhull.com>
2016-09-12 19:39:48 +0300

    l2.06 Added Category controller

---
+++ controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Boris
+ * Date: 12.09.2016
+ * Time: 20:11
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class AppController extends Controller
+{
+
+}

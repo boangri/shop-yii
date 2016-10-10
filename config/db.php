06fa8ee5754e272c53f540ccc1516b0a12317cc8
Boris Gribovskiy <boris.gribovskiy@cyberhull.com>
2016-10-10 16:19:04 +0300

    master: Changer db name to shop_yii

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=shop-yii',
+    'dsn' => 'mysql:host=localhost;dbname=shop_yii',
     'username' => 'magento',
     'password' => 'magento',
     'charset' => 'utf8',
